4a1b6c6b45c7fa7099b5a3ee2c8235bc0e14ce23
raqsanta <eahisla@gmail.com>
2022-06-29 21:30:44 -0300

    Add sample route

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 const express = require('express')
+const app = express()
+require('dotenv/config')
+
+app.get('/', (req,res) => {
+    res.send('Testando...')
+})
+
+app.listen(process.env.SERVER_PORT, () => {
+    console.log(`Servidor iniciado na porta ${process.env.SERVER_PORT}`)
+})
 
 //O server será iniciado aqui

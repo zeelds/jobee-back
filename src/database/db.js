c85f32ce9d1f5627e622e0516324f185eabb039d
raqsanta <eahisla@gmail.com>
2022-07-04 21:27:32 -0300

    Update db.js

--- src/database/db.js
+++ src/database/db.js
@@ -1,24 +1,6 @@
 const {Sequelize} = require('sequelize')
 const dbConfig = require('../config/dbconfig');
 
-/*
-const Accessibility = require('../models/Accessibility');
-const Article = require('../models/Article');
-const Comment = require('../models/Comment');
-const Login = require('../models/Login');
-const Review = require('../models/Review');
-const User = require('../models/User');
-*/
-
 const database = new Sequelize(dbConfig);
-
-/*
-Accessibility.init(database)
-Article.init(database)
-Comment.init(database)
-Login.init(database)
-Review.init(database)
-User.init(database)
-*/
 
 module.exports = database;

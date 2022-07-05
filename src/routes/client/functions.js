550b7ce2f0676d51b1d9f2fb241778476ac736f4
raqsanta <eahisla@gmail.com>
2022-07-05 02:06:24 -0300

    Update validation

--- src/routes/client/functions.js
+++ src/routes/client/functions.js
@@ -1,8 +1,82 @@
 
+const validator = require('email-validator')
+
 function creationValidate(values){
 
     const {email, password, repassword, name, birthday, gender} = values
     
-    //Validação aqui
+    let errors = []
+    
+    if(!email){
+        errors.push({
+            where: 'email',
+            content: 'Este campo não pode estar em branco.'
+        })
+    }
+    else if(!validator.validate(email)){
+        errors.push({
+            where: 'email',
+            content: 'O e-mail precisa ser válido.'
+        })
+    };
+
+    if(!password){
+        errors.push({
+            where: 'password',
+            content: 'Este campo não pode estar em branco.'
+        })
+    }
+    else if(password.length <= 5){
+        errors.push({
+            where: 'password',
+            content: 'A senha deve conter mais de 5 caracteres.'
+        })
+    };
+
+    if(!repassword){
+        errors.push({
+            where: 'repassword',
+            content: 'Este campo não pode estar em branco.'
+        })
+    };
+
+    if(!name){
+        errors.push({
+            where: 'name',
+            content: 'Este campo não pode estar em branco.'
+        })
+    };
+
+    if(!birthday){
+        errors.push({
+            where: 'birthday',
+            content: 'Este campo não pode estar em branco.'
+        })
+    };
+
+    if(!gender){
+        errors.push({
+            where: 'gender',
+            content: 'Este campo não pode estar em branco.'
+        })
+    }
+    else if(!['Masculino', 'Feminino', 'Prefiro não dizer', 'Outros'].includes(gender)){
+        errors.push({
+            where: 'gender',
+            content: 'Você selecionou um gênero desconhecido.'
+        })
+    };
+
+    if(password != repassword){
+        errors.push({
+            where: 'password',
+            content: 'As senhas não coincidem.'
+        },{
+            where: 'repassword',
+            content: 'As senhas não coincidem.'
+        })
+    };
+
+    return errors
 
 }

6389a74797156e127c1e1daa9744e87ccd47d683
raqsanta <eahisla@gmail.com>
2022-07-04 21:24:58 -0300

    Update start.js

--- src/database/start.js
+++ src/database/start.js
@@ -6,12 +6,3 @@ const Comment = require('../models/Comment');
 const Login = require('../models/Login');
 const Review = require('../models/Review');
 const User = require('../models/User');
-
-/*
-Accessibility.init(database)
-Article.init(database)
-Comment.init(database)
-Login.init(database)
-Review.init(database)
-User.init(database)
-*/

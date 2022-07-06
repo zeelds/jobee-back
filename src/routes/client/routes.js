5741bae1dc018abeb55bb8e8af8adb737df545be
raqsanta <eahisla@gmail.com>
2022-07-05 21:28:16 -0300

    Update email verification

--- src/routes/client/routes.js
+++ src/routes/client/routes.js
@@ -8,6 +8,15 @@ router.get('/', verifyJWT, (req,res) => {
     return res.json({type: 'success', message: 'Você está autenticado!'})
 })
 
+router.get('/teste', (req,res) => {
+    
+    const teste = jwt.sign({ user_id: '8d2bc591-9aca-4f86-ac3a-d59e02cddcd3' },
+        process.env.SECRET_JWT)
+
+    return res.json(teste)
+
+})
+
 router.post('/register', async (req, res) => {
 
     const { email, password, repassword, name, birthday, gender } = req.body
@@ -28,6 +37,9 @@ router.post('/register', async (req, res) => {
 
         const newLogin = UserController.createLogin(email, password, user_id)
         const newAcessibility = UserController.createAccessibility(false, 'Ele', 'Nenhum', user_id)
+
+        const verifyToken = jwt.sign(user_id, process.env.SECRET_JWT)
+        //vai ser enviado pro email
 
         return res.json({ message: user_id + ' foi registrado com sucesso!' })
 
@@ -53,6 +65,11 @@ router.post('/auth', async (req, res) => {
         &&
         password == combination.data.password
     ) {
+
+        if(!combination.isverified){
+            return res.json({ auth: false, type: 'error', message: 'A conta precisa ser ativada, verifique o seu email!' })
+        }
+
         const token = jwt.sign(
             { user_id: combination.data.user_id },
             process.env.SECRET_JWT,
@@ -66,15 +83,19 @@ router.post('/auth', async (req, res) => {
 
 })
 
-router.get('/verify/:id', async (req, res) => {
+router.get('/verify/:token', async (req, res) => {
 
     //Um botão do front pro email que manda de volta pro front, mas passando um 
     //Parâmetro na url que assim que a página abre, envia uma req pra essa rota
     //E confirma a conta :-)
 
-    const user_id = req.params.id
+    const token = req.params.token
 
-    const updatedVerify = await UserController.updateVerifiedStatus({ isverified: true, user_id: user_id })
+    const user_id = jwt.verify(token, process.env.SECRET_JWT, (err, decoded)=>{
+        return decoded.user_id
+    })
+
+    const updatedVerify = await UserController.updateVerifiedStatus({ isverified: true, user_id: user_id.toString() })
 
     if (updatedVerify.data == 0) {
         return res.json({ type: 'error', message: 'Essa conta não pôde ser verificada no momento.' })
@@ -83,7 +104,6 @@ router.get('/verify/:id', async (req, res) => {
     return res.json({ type: 'success', message: 'Você teve sua conta verificada!' })
 
 })
-
 
 router.post('/redefine-password', async (req, res) => {
 
